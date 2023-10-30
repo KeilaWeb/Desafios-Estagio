05496d198521604ee880e570173dafc4bd68f157
Keila D. Barreto <91154547+KeilaWeb@users.noreply.github.com>
2023-10-30 11:53:08 -0300

    enfim funcionando

--- 01-DESAFIO-CACLCULADORA/script.js
+++ 01-DESAFIO-CACLCULADORA/script.js
@@ -1,70 +1,65 @@
 //seleção dos elementos (atraves do DOM)
-const preverOperacaoTexto = document.querySelector("#prever-resultado"); // Visor do cálculo de previsão
-const calculando = document.querySelector("#calculando"); // Visor do cálculo sendo digitado
+const preverOperacao = document.querySelector("#prever-resultado"); // Visor do cálculo de previsão(superior)
+const digitandoOperacao = document.querySelector("#calculando"); // Visor do cálculo sendo digitado(inferior)
 const botoes = document.querySelectorAll("#gradeBotao button"); // Todos os botões da calculadora
 
 
-//Lógica da calcularora em JS POO
+//POO JS - Lógica da calcularora
 class Calculado {
-    constructor(preverOperacaoTexto, calculando){ //valores de fluxo da operação
-        this.preverOperacaoTexto = preverOperacaoTexto; //Propriedades do objeto para não trabalhar diretamente com o DOM
-        this.calculando = calculando;
-        this.calcula = ""; //Valor que o usuário está digitando
+    constructor(preverOperacao, digitandoOperacao){ //valores de fluxo da operação
+        this.preverOperacao = preverOperacao; //Propriedades do objeto para não trabalhar diretamente com o DOM
+        this.digitandoOperacao = digitandoOperacao;
+        this.digitando = ""; //Valor que o usuário está digitando
     }
 
-    //adiciona digitos da calculadora
-    adicionaDigito(digito){
-        //Verifica se o último caractere no visor é um ponto e se já existe um ponto (função da lógica do IF de botoes.foreach)
-        if (digito === "." && this.calculando.innerText.includes(".")) {
+   
+    adicionaDigito(digito){//Verifica se o último caractere no visor é um ponto e se já existe um ponto (função da lógica do IF foreach)
+        if (digito === "." && this.digitandoOperacao.innerText.includes(".")) {
             return;
         }
-
-        this.calcula = digito; // adiciona o dígito ao visor
-        this.atualizaTela()  //Atualiza o visor
+        this.digitando = digito; // adiciona o dígito ao visor
+        this.atualizaTela()  //Atualiza o visor        
     }
 
     //processa as operações (função da logica do ELSE de botoes.foreach)
-    valoresOPeradores(operacao){
-        //Checa se o valor no visor é vazio (empty) e a operação não é "C"
-        if(calculando.innerText === "" && operacao !== "C"){
-            //Mudança de operação
-            if(this.preverOperacaoTexto.innerText !== ""){
+    processaOperacao(operacao){//Checar se o valor no visor é vazio (empty) e a operação não é "C"
+        if(digitandoOperacao.innerText === "" && operacao !== "C"){//Mudança de operação
+            if(this.preverOperacao.innerText !== ""){
                 this.mudaOperacao(operacao);
             }
             return;
-        }
-
+        }        
 
         //Pegando os valores atuais e os recorrente
         let valorDaOperacao
-        const prever = +this.preverOperacaoTexto.innerText.split(" ")[0]; //metodo split(para a string do vetor ser ordenado para uma sustring em um array e retorna array, soma na previsão)
-        const recorrente = +this.calculando.innerText //valor passado anterior para valor numerico
+        const prever = +this.preverOperacao.innerText.split(" ")[0]; //metodo split(para a string do vetor ser ordenado para uma substring em um array e retorna array, soma na previsão)
+        const recorrente = +this.digitandoOperacao.innerText //valor passado anterior para valor numerico
 
         switch(operacao){ 
             case "+":
-                valorDaOperacao = prever + recorrente;;
+                valorDaOperacao = prever + recorrente;
                 this.atualizaTela(valorDaOperacao, operacao, recorrente, prever);
                 break
             case "-":
-                valorDaOperacao = prever - recorrente;;
+                valorDaOperacao = prever - recorrente;
                 this.atualizaTela(valorDaOperacao, operacao, recorrente, prever);
                 break
             case "/":
-                valorDaOperacao = prever / recorrente;;
+                valorDaOperacao = prever / recorrente;
                 this.atualizaTela(valorDaOperacao, operacao, recorrente, prever);
                 break
             case "*":
-                valorDaOperacao = prever * recorrente;;
+                valorDaOperacao = prever * recorrente;
                 this.atualizaTela(valorDaOperacao, operacao, recorrente, prever);
                 break
             case "DEL":
-                this.processaDel();
+                this.limpaUmDEL();
                 break
             case "CE":
-                this.limpaOperacaoRecorrente();
+                this.limpaOperacaoRecorrenteCE();
                 break
             case "C":
-                this.limpaTodaOperacao();
+                this.limpaTodaOperacaoC();
                 break
             case "=":
                 this.resultadoDaOperacao();
@@ -72,76 +67,68 @@ class Calculado {
             default: //retorno para operação que não é válida
                 return
         }
-    }
-        
+    }        
 
-    //muda os valores da na tela
-    atualizaTela(
-        valorDaOperacao = null, 
-        operacao = null, 
-        prever = null, 
-        recorrente = null
-        ){ //metodo para ter acesso ao this.calculando
+    atualizaTela(valorDaOperacao = null, operacao = null, recorrente = null, prever = null){ //metodo para ter acesso ao this.digitandoOperacao
         if (valorDaOperacao === null) {
-            this.calculando.innerText = this.calcula //concatenando os valores
-        }else {
-            //checar se o valor é zero, se não adiciona o valor recorrente
+            this.digitandoOperacao.innerText += this.digitando //concatena os valores
+        }else { //checar se o valor é zero, se não adiciona o valor la pra cima          
             if(prever === 0){
                 valorDaOperacao = recorrente;
-            }
+            }//Jogando o valor da parte de baixo para cima            
+            this.preverOperacao.innerText = `${valorDaOperacao} ${operacao}`; //aparece o valor em cima
+            this.digitandoOperacao.innerText = ""; //limpa o valor debaixo no visor
+        }
+        this.resultadoInfinty()
+    }
 
-            //Jogando o valor da parte debaixo para cima
-            this.preverOperacaoTexto.innerText = `${valorDaOperacao} ${operacao}`; //aparece o valor em cima
-            this.calculando.innerText = ""; //limpa o valor debaixo
+    resultadoInfinty(){
+        if (preverOperacao.innerText === "Infinity /") {
+            alert("[ERRO!] Adicione uma operação válida!")
+            this.preverOperacao.innerText = "";
         }
     }
 
-    //Mudando a operação
     mudaOperacao(operacao){
         const operacaoMatematica = ["*", "-", "+", "/"];
         if (!operacaoMatematica.includes(operacao)) {
             return;
-        }
-        //substitui o ultimo operador
-        this.preverOperacaoTexto.innerText = this.preverOperacaoTexto.innerText.slice(0, -1) + operacao
+        }//substitui o ultimo operador
+        this.preverOperacao.innerText = this.preverOperacao.innerText.slice(0, -1) + operacao
     }
 
-    //Deleta o ultimo dígito
-    processaDel() {
-        this.calculando.innerText = this.calculando.innerText.slice(0, -1)
+    limpaUmDEL() {
+        this.digitandoOperacao.innerText = this.digitandoOperacao.innerText.slice(0, -1)
     }
 
-    //limpa a operação que esta sendo digitada abaixo
-    limpaOperacaoRecorrente(){
-        this.calculando.innerText = ""
+    limpaOperacaoRecorrenteCE(){
+        this.digitandoOperacao.innerText = ""
     }
 
     //Limpa a operação de cima e a de baixo
-    limpaTodaOperacao(){
-        this.preverOperacaoTexto.innerText = ""
-        this.calculando.innerText = ""
+    limpaTodaOperacaoC(){
+        this.preverOperacao.innerText = ""
+        this.digitandoOperacao.innerText = ""
     }
 
     // Resultado da Operação
-    resultadoDaOperacao(){
-        const operacao = preverOperacaoTexto.innerText.split(" ")[1]
-        this.valoresOPeradores(operacao)
+    resultadoDaOperacao(){        
+        const operacao = preverOperacao.innerText.split(" ")[1]
+        this.processaOperacao(operacao)
     }
 }
 
-
-const calculadora = new Calculado(preverOperacaoTexto, calculando) //instancia para executar os métoodos de entrada da lógica (if e else)
-
+const calculadora = new Calculado(preverOperacao, digitandoOperacao) //instancia para executar os métoodos de entrada da lógica (if e else)
 
 //Iterando sobre todos os botões e adicionando um evento de clique a cada um
-botoes.forEach((botao /*nome individual de cada botão*/) => {
-    botao.addEventListener/*adicionando um evento*/("click", (evento) => { //Quando um botão é clicado, este bloco de código é executado
-        const value = evento.target.innerText //Obtendo o texto do botão clicado
+botoes.forEach((botao) => {
+    botao.addEventListener("click", (e) => { //Quando um botão é clicado, este bloco de código é executado
+        const value = e.target.innerText; //Obtendo o texto do botão clicado
         //Verificando se o valor é um número ou um ponto
         if (+value /*converte valor em numero*/ >= 0 || value === ".") { //// Se for um número ou um ponto, chame o método para adicionar o dígito à calculadora
             calculadora.adicionaDigito(value)
         }else { // Se não for um número, é reconhecido como uma operação, e chamamos o método correspondente
-            calculadora.valoresOPeradores(value)
+            calculadora.processaOperacao(value)
         }
     })
 })
